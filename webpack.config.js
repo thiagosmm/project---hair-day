1d345a0b0b9f80677862752d2761a62ade712899
thiagosmm <thiagosmmonteiro@gmail.com>
2025-02-24 16:00:29 -0300

    web pack dev server

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  target: "web",
+  mode: "development",
+
+  entry: path.resolve(__dirname, "src", "main.js"),
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), 
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  }
+}
