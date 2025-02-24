1150f57fd5d318014be96f27ec59f19726f021fc
thiagosmm <thiagosmmonteiro@gmail.com>
2025-02-24 16:05:50 -0300

    htmml + faviicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   target: "web",
@@ -18,5 +19,10 @@ module.exports = {
     port: 3000,
     open: true,
     liveReload: true,
-  }
+  },
+
+  plugins: [new htmlWebpackPlugin({
+    template: path.resolve(__dirname, "index.html"),
+    favicon: path.resolve("src", "assets", "scissors.svg"),
+  })],
 }
